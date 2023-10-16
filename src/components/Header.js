f057781368a7b29b1c6d35cd45427225225efde9
abdelaliem <abdelaliieem@gmail.com>
2023-10-16 20:43:30 +0300

    v1.1.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-function Header() {
-  return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
-      <h1>The React Quiz</h1>
-    </header>
-  );
+// function Header() {
+//   return (
+//     <header className='app-header'>
+//       <img src='logo512.png' alt='React logo' />
+//       <h1>The React Quiz</h1>
+//     </header>
+//   );
+// }
+
+import React,{ Component } from "react";
+
+class Header extends Component {
+  
+  render() {
+    return (
+      <header className="app-header">
+        <img src="logo512.png" alt="React logo" />
+        <h1>The React Quiz</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
